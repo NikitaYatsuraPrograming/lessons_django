1bb3fc8a242b4ec24b93f7e2295606de04a0d960
Yatsura Nikita <yatsuranikita@gmail.com>
2023-01-08 11:58:54 +0200

    first commit

---
+++ backend/applications/user_module/models.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    is_seller = models.BooleanField(default=False, verbose_name='Seller')
